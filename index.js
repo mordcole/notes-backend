6b6eb2ac7caad7d18d2512f17ec991ee6f10e6d0
mordcoleman <105605981+Mcolemangit@users.noreply.github.com>
2026-01-21 11:04:44 -0500

    added linter

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -55,6 +55,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then((savedNote) => {
     response.json(savedNote)
   })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -88,6 +89,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
